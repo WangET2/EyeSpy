182d7d4aabfaa287beb3cb30300c44016095b983
WangET2 <etwang2@gmail.com>
2025-03-22 15:12:18 -0700

    Began transfer of pipeline from filehandler.py to processing.py with optimizations/cleaning. Added base class for FlyImage

--- images/flyimage.py
+++ images/flyimage.py
@@ -1,47 +1,56 @@
+import abc
+
 import numpy as np
 from pathlib import Path
 import czifile
 
+@ abc.ABC
 class FlyImage:
+    @property
+    def array(self):
+        return self._array
+
+    @property
+    def scaling(self):
+        return self._scaling
+
+    @property
+    def white_point(self):
+        return self._white_point
+
+class OtherFlyImage(FlyImage):
     """Used for persistent storage of image arrays, preventing errors
     caused by attempting to follow paths that no longer exist."""
 
-    def __init__(self, array: np.ndarray, scaling: int):
+    def __init__(self, array: np.ndarray, scaling: int, white_point: int):
         #Handle reading the array from the file upstream, for flexibility.
         self._array = array
         #Scaling is provided by the config.
         self._scaling = scaling
-
-    @property
-    def array(self) -> np.ndarray:
-        """Array representation of the image."""
-        return self._array
-
-    @property
-    def scaling(self) -> int:
-        """Scaling of the image, in µm/pixel."""
-        return self._scaling
+        #White point is provided by the config.
+        self._white_point = white_point
 
 
-class CziFlyImage:
+class CziFlyImage(FlyImage):
     """CZI fly image. Stores image arrays and scaling persistently
     upon object creation, but all other metadata is lost."""
 
     def __init__(self, image_directory: Path):
-        img = czifile.CziFile(image_directory)
+        try:
+            img = czifile.CziFile(image_directory)
+        except:
+            raise FileNotFoundError(f'Could not find {image_directory}.')
 
         #TODO clean up this mess...
         metadata = img.metadata(raw=False)
         self._scaling = str(metadata['ImageDocument']['Metadata']['Scaling']['Items']['Distance'][0]['Value'])
         self._scaling = float(self._scaling[:self._scaling.index('e')])
-        self._array = img.asarray()[:, 1, 1, :]
 
-    @property
-    def array(self) -> np.ndarray:
-        """Array representation of the image."""
-        return self._array
+        try:
+            self._array = img.asarray()[:, 1, 1, :]
+        except:
+            raise ValueError("File format was not CZI or could not be loaded as expected.")
 
-    @property
-    def scaling(self):
-        """Scaling of image, in µm/pixel."""
-        return self._scaling
+        #TODO Check metadata for white point
+        self._white_point = 0
+
--- images/processing.py
+++ images/processing.py
@@ -3,18 +3,46 @@
 # Generate statistically sound decision boundary for thresholding
 # Test/optimize
 import numpy as np
-
+import cv2 as cv
 from images.flyimage import FlyImage, CziFlyImage
 
-def get_mean_fluorescence(image: FlyImage|CziFlyImage):
+class Processor:
+    pass
+
+def get_mean_fluorescence(image: FlyImage|CziFlyImage, threshold1: float, threshold2: float):
     processed_array = np.copy(image.array)
 
-    processed_array = _proportional_scale(processed_array)
+    #Scaling each value in the array proportionally to its 95th percentile of brightness
+    processed_array = _proportional_scale(processed_array, image.white_point)
+
+    #Apply thresholding to the array
+    processed_array = np.where(processed_array > threshold1,1, 0)
+
+    #High data precision is no longer necessary; reduce bit depth of the array for OpenCV compatability
+    processed_array = cv.normalize(processed_array, dst=None, alpha=0, beta=255,
+                                   norm_type=cv.NORM_MINMAX, dtype= cv.CV_8U)
+
+    #Morphological Operations account for potential noise in the binary mask
+    processed_array = cv.morphologyEx(processed_array, cv.MORPH_OPEN, np.ones((5, 5), np.uint8))
+    processed_array = cv.morphologyEx(processed_array, cv.MORPH_CLOSE, np.ones((5, 5), np.uint8))
+
+    #The distance transform allows us to weight pixels relative to other 'on' pixels.
+    processed_array = cv.distanceTransform(processed_array, cv.DIST_L2, 5)
+
+    #Thresholding is applied, again. This allows us to filter low-connectivity regions of the image.
+    processed_array = np.where(processed_array > threshold2, 1, 0)
+
+    #Morphological operations, for the same reasons as above.
 
 
     del image
     return
 
-def _proportional_scale(arr: np.ndarray) -> np.ndarray:
+def _proportional_scale(arr: np.ndarray, white_point: int) -> np.ndarray:
     """Scales the image proportionally to the 95th percentile
     of pixel brightness in the image array."""
+    ubound = np.percentile(arr, 95)
+    def scale(x):
+        return ubound if x > ubound else (x * white_point) / ubound
+    vectorize_scale = np.vectorize(scale)
+    return vectorize_scale(arr)
