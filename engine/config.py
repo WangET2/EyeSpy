5395c0b1ef46a4a8b37fef2a9b4a24c3c834df7c
WangET2 <etwang2@gmail.com>
2025-03-26 10:14:57 -0700

    Created Config class and added properties.

--- engine/config.py
+++ engine/config.py
@@ -2,5 +2,39 @@
 # Add proper config file handling
 # Interface with gui.config_gui
 
+import os
+from configparser import ConfigParser
+
 class Config:
-    pass
+    def __init__(self):
+        config = ConfigParser()
+        if not 'options.ini' in os.listdir():
+            configfile = open('options.ini', 'w')
+            config['DEFAULT'] = {'Directory': 'None',
+                                 'Scaling': '5',
+                                 'White Point': '128'}
+            config['ADVANCED'] = {'Threshold': '100'}
+            config.write(configfile)
+            configfile.close()
+        config.read('options.ini')
+        self._directory = config['DEFAULT']['Directory']
+        self._scaling = config['DEFAULT']['Scaling']
+        self._white_point = config['DEFAULT']['WhitePoint']
+        self._threshold = config['ADVANCED']['Threshold']
+
+    @property
+    def directory(self):
+        return self._directory
+
+    @property
+    def scaling(self):
+        return self._scaling
+
+    @property
+    def white_point(self):
+        return self._white_point
+
+    @property
+    def threshold(self):
+        return self._threshold
+
