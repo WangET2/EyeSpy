4859880f69ced4ecd03f4b9196e88bf93497ec92
Ethan W <etwang2@gmail.com>
2025-06-16 02:18:53 -0700

    Added stable_read to heuristically ensure that an image is completely written before it is accessed. Added some edge case guards

--- engine/queue.py
+++ engine/queue.py
@@ -4,6 +4,7 @@ from collections import deque
 from abc import ABC, abstractmethod
 import numpy as np
 import czifile
+import time
 
 
 '''
@@ -30,7 +31,8 @@ class BaseQueue(ABC):
         return not self._deque
 
     def dequeue(self) -> None:
-        self._deque.popleft()
+        if not self.is_empty():
+            self._deque.popleft()
 
     def update(self) -> None:
         for val in self._directory.iterdir():
@@ -42,7 +44,7 @@ class BaseQueue(ABC):
         pass
 
     @abstractmethod
-    def front(self) -> np.ndarray:
+    def front(self) -> np.ndarray | None:
         pass
 
 class FileQueue(BaseQueue):
@@ -52,25 +54,49 @@ class FileQueue(BaseQueue):
         self._deque.append(val)
         self._seen.add(val)
 
-    def front(self) -> np.ndarray:
-        imgpath = self._directory / self._deque[0]
-        try:
-            return czifile.imread(imgpath)
-        except FileNotFoundError:
-            print(f"Error accessing file: {self._deque[0]} no longer exists or cannot be accessed.")
-            self.dequeue()
-            return self.front()
+    def front(self) -> np.ndarray | None:
+        while not self.is_empty():
+            imgpath = self._directory / self._deque[0]
+            imgarr = stable_read(imgpath)
+            if imgarr:
+                return imgarr
+            else:
+                self.dequeue()
+        return None
 
 class ImageQueue(BaseQueue):
     def enqueue(self, val: str) -> None:
         if val in self._seen:
             return
-        try:
-            imgarr = czifile.imread(self._directory / val)
+        imgpath = self._directory / val
+        imgarr = stable_read(imgpath)
+        if imgarr:
             self._deque.append(imgarr)
             self._seen.add(val)
-        except FileNotFoundError:
-            print(f"Error accessing file: {val} no longer exists or cannot be accessed.")
 
-    def front(self) -> np.ndarray:
+    def front(self) -> np.ndarray | None:
         return self._deque[0] if not self.is_empty() else None
+
+def stable_read(img_path: Path, max_attempts: int=10, delay_s:float=0.2, required_stable:int=3) -> np.ndarray | None:
+    attempts = 0
+    stable_count = 0
+    try:
+        while attempts <= max_attempts:
+            if not img_path.exists():
+                return None
+            initial_size = img_path.stat().st_size
+            time.sleep(delay_s)
+            current_size = img_path.stat().st_size
+            if initial_size == current_size and current_size > 0:
+                stable_count += 1
+            else:
+                stable_count = 0
+            if stable_count >= required_stable:
+                break
+            attempts += 1
+        if attempts > max_attempts:
+            return None
+        return czifile.imread(img_path)
+    except FileNotFoundError:
+        print(f"Error accessing file: {img_path} no longer exists or cannot be accessed.")
+    return None
