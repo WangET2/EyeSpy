ff15748785a7a15beb202ff6ad6c1895dc494b89
Ethan W <etwang2@gmail.com>
2025-06-23 08:52:56 -0700

    Updated UI with tooltips and additional buttons

--- src/gui/processing_ui.py
+++ src/gui/processing_ui.py
@@ -11,15 +11,20 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_ProcessingWindow(object):
     def setupUi(self, ProcessingWindow):
         ProcessingWindow.setObjectName("ProcessingWindow")
-        ProcessingWindow.resize(429, 298)
+        ProcessingWindow.resize(429, 329)
         self.centralwidget = QtWidgets.QWidget(ProcessingWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
         self.gridLayout.setObjectName("gridLayout")
+        self.exit_button = QtWidgets.QPushButton(self.centralwidget)
+        self.exit_button.setObjectName("exit_button")
+        self.gridLayout.addWidget(self.exit_button, 1, 0, 1, 1)
+        spacerItem = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.gridLayout.addItem(spacerItem, 1, 1, 1, 1)
         self.output_textbox = QtWidgets.QTextBrowser(self.centralwidget)
         self.output_textbox.setAcceptRichText(True)
         self.output_textbox.setObjectName("output_textbox")
-        self.gridLayout.addWidget(self.output_textbox, 0, 0, 1, 1)
+        self.gridLayout.addWidget(self.output_textbox, 0, 0, 1, 2)
         ProcessingWindow.setCentralWidget(self.centralwidget)
         self.statusbar = QtWidgets.QStatusBar(ProcessingWindow)
         self.statusbar.setObjectName("statusbar")
@@ -31,6 +36,7 @@ class Ui_ProcessingWindow(object):
     def retranslateUi(self, ProcessingWindow):
         _translate = QtCore.QCoreApplication.translate
         ProcessingWindow.setWindowTitle(_translate("ProcessingWindow", "MainWindow"))
+        self.exit_button.setText(_translate("ProcessingWindow", "Save and Quit"))
         self.output_textbox.setHtml(_translate("ProcessingWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
